81e9dad0b348f9a43934a802f080b013bc9e5f38
Sonenka <sonenkkaa@gmail.com>
2026-03-09 21:04:37 +0300

    storybook init

---
+++ storybook-static/assets/formatter-EIJCOSYU-BXOAu3xY.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-gXaQUI_J.js";import{E as o}from"./iframe-BI1-08-x.js";import"./preload-helper-C1FmrZbK.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
