d817db76142f63b17b41af2e3464466b7ebd7ce2
Sonenka <sonenkkaa@gmail.com>
2026-04-24 22:47:01 +0300

    delete nonexited components

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,5 @@ export { InputType, type InputProps, type InputEmits } from './components/input/
 export { default as EosButton } from './components/button/Button.vue'
 export { ButtonSize, ButtonVariant, type ButtonProps } from './components/button/types'
 
-export { default as EosSelect } from './components/select/Select.vue'
-
-export { default as EosAlert } from './components/alert/Alert.vue'
-export { default as EosBadge } from './components/badge/Badge.vue'
-export { default as EosBreadcrumb } from './components/breadcrumb/Breadcrumb.vue'
-export { default as EosCard } from './components/card/Card.vue'
-export { default as EosSpinner } from './components/spinner/Spinner.vue'
-
 // Icons (auto-generated from src/icons/*.svg)
 export * from './components/icons'
