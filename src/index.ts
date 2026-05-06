99e88c7df1c1530934ffe4b7c240456c7ecefd31
Sonenka <sonenkkaa@gmail.com>
2026-05-06 17:22:42 +0300

    add table component

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,15 @@ export { InputType, type InputProps, type InputEmits } from './components/input/
 export { default as EosButton } from './components/button/Button.vue'
 export { ButtonSize, ButtonVariant, type ButtonProps } from './components/button/types'
 
+export { default as EosTable } from './components/table/Table.vue'
+export type {
+  TableProps,
+  TableColumn,
+  TableCell,
+  TableRow,
+  TableEmits
+} from './components/table/types'
+
 // Icons (auto-generated from src/icons/*.svg)
 export * from './components/icons'
 
