e070a1b7f2699eebdd530636946ff8fb22212e24
Sonenka <sonenkkaa@gmail.com>
2026-05-07 00:10:23 +0300

    update prettier

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -9,77 +9,82 @@
       :class="inputClasses"
       @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
     />
-    <div v-if="error" class="input__error">{{ error }}</div>
+    <div
+      v-if="error"
+      class="input__error"
+    >
+      {{ error }}
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { computed } from 'vue'
-  import { InputProps, InputType, InputEmits } from './types'
+import { computed } from 'vue'
+import { InputProps, InputType, InputEmits } from './types'
 
-  const props = withDefaults(defineProps<InputProps>(), {
-    type: InputType.Text,
-    modelValue: '',
-    disabled: false,
-    placeholder: 'Введите текст'
-  })
+const props = withDefaults(defineProps<InputProps>(), {
+  type: InputType.Text,
+  modelValue: '',
+  disabled: false,
+  placeholder: 'Введите текст'
+})
 
-  const emit = defineEmits<InputEmits>()
+const emit = defineEmits<InputEmits>()
 
-  const inputClasses = computed(() => ({
-    input_error: props.error,
-    input_disabled: props.disabled
-  }))
+const inputClasses = computed(() => ({
+  input_error: props.error,
+  input_disabled: props.disabled
+}))
 </script>
 
 <style lang="scss" scoped>
-  .input {
-    padding: var(--eos-space-s) var(--eos-space-m);
-    font-size: var(--eos-font-size-m);
-    border: 2px solid var(--eos-color-primary-300);
-    border-radius: var(--eos-radius-m);
-    outline: none;
-    color: var(--eos-color-primary-800);
+.input {
+  padding: var(--eos-space-s) var(--eos-space-m);
+  font-size: var(--eos-font-size-m);
+  border: 2px solid var(--eos-color-primary-300);
+  border-radius: var(--eos-radius-m);
+  outline: none;
+  color: var(--eos-color-primary-800);
+
+  &:focus {
+    border-color: var(--eos-color-primary-500);
+  }
+
+  &::placeholder {
+    color: var(--eos-color-primary-200);
+  }
+
+  &_error {
+    border-color: var(--eos-color-error);
+    background-color: var(--eos-color-error-light);
 
     &:focus {
-      border-color: var(--eos-color-primary-500);
+      border-color: var(--eos-color-error);
     }
+  }
+
+  &_disabled {
+    background-color: var(--eos-color-primary-100);
+    color: var(--eos-color-primary-600);
+    cursor: not-allowed;
+    opacity: 0.6;
 
     &::placeholder {
       color: var(--eos-color-primary-200);
     }
-
-    &_error {
-      border-color: var(--eos-color-error);
-      background-color: var(--eos-color-error-light);
-
-      &:focus {
-        border-color: var(--eos-color-error);
-      }
-    }
-
-    &_disabled {
-      background-color: var(--eos-color-primary-100);
-      color: var(--eos-color-primary-600);
-      cursor: not-allowed;
-      opacity: 0.6;
-
-      &::placeholder {
-        color: var(--eos-color-primary-200);
-      }
-    }
-
-    &__wrapper {
-      display: flex;
-      flex-direction: column;
-      gap: var(--eos-space-s);
-      font-family: var(--eos-font-family);
-    }
-
-    &__error {
-      font-size: var(--eos-font-size-s);
-      color: var(--eos-color-error);
-      line-height: var(--eos-line-height-normal);
-    }
   }
+
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: var(--eos-space-s);
+    font-family: var(--eos-font-family);
+  }
+
+  &__error {
+    font-size: var(--eos-font-size-s);
+    color: var(--eos-color-error);
+    line-height: var(--eos-line-height-normal);
+  }
+}
 </style>
