023f551e9d22241864f11d1d78df097911928d8b
Sonenka <sonenkkaa@gmail.com>
2026-05-18 03:53:02 +0300

    add edit icon

---
+++ src/components/icons/EditIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+  >
+    <g clip-path="url(#a)">
+      <path
+        fill="currentColor"
+        d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1q-.15.15-.15.36M20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75z"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path
+          fill="currentColor"
+          d="M0 0h24v24H0z"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  width: 100%;
+  height: 100%;
+  display: block;
+  flex-shrink: 0;
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as EosAccountIcon } from './AccountIcon.vue'
 export { default as EosChevronIcon } from './ChevronIcon.vue'
 export { default as EosCloseIcon } from './CloseIcon.vue'
+export { default as EosEditIcon } from './EditIcon.vue'
 export { default as EosHomeIcon } from './HomeIcon.vue'
 export { default as EosLoopIcon } from './LoopIcon.vue'
